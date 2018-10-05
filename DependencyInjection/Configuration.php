10c45e40fb419da42afe4bc19894e52a8eec9f05
Daniel Leeb <dl@loremipsum.at>
2018-10-05 13:41:28 +0200

    Rename root tree node

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -10,7 +10,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode    = $treeBuilder->root('action_factory');
+        $rootNode    = $treeBuilder->root('lorem_ipsum_action_logger');
 
         $rootNode
             ->children()
