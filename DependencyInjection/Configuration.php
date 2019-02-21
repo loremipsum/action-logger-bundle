d1278ea8789207cd5479cd29951238e1131c8a13
Daniel Leeb <dl@loremipsum.at>
2019-02-21 19:05:58 +0100

    Fix TreeBuilder deprecation

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -9,8 +9,13 @@ class Configuration implements ConfigurationInterface
 {
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode    = $treeBuilder->root('lorem_ipsum_action_logger');
+        $treeBuilder = new TreeBuilder('lorem_ipsum_action_logger');
+        if (method_exists($treeBuilder, 'getRootNode')) {
+            $rootNode = $treeBuilder->getRootNode();
+        } else {
+            // BC layer for symfony/config 4.1 and older
+            $rootNode = $treeBuilder->root('lorem_ipsum_action_logger');
+        }
 
         $rootNode
             ->children()
